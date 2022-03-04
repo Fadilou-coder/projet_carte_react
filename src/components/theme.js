f80ea0ca70e89c56482dbf40a260cd16747cac7a
Omar faye <omar.fayeomzolive@gmail.com>
2022-03-04 08:25:46 +0000

    commit

---
+++ src/components/theme.js
@@ -0,0 +1,21 @@
+import {createTheme} from "@material-ui/core";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#05888A",
+            green: "#c8ecf1",
+            white:"#FFFFFF",
+            grey: "#E5E5E5",
+            black: "#000000",
+            vert:"#51BFD0"
+        },
+        secondary : {
+            main:"#51BFD0",
+            orange:"#BC6602",
+            labelColor:"#756F86"
+        }
+    },
+});
+
+export default theme;
