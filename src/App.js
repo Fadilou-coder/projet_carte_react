32622c1f7abf02888b318cfb2938d9a67f1e94a9
Omar faye <omar.fayeomzolive@gmail.com>
2022-03-07 11:25:36 +0000

    topbar & leftbar oki

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
-import LoginComponent from "./components/loginComponent/LoginComponent";
 import Visites from "./components/visites/Visites";
 import Layout from "./components/layout/Layout";
 import ListApprenant from "./components/apprenant/ListApprenant";
@@ -13,8 +12,8 @@ function App() {
         <Router>
            {/* <Layout>*/}
               <Switch>
-                  <Route exact path="/" component={LoginComponent}/>
-                  <Route  path="/login" component={LoginComponent}/>
+                  {/*<Route exact path="/" component={LoginComponent}/>
+                  <Route  path="/login" component={LoginComponent}/>*/}
                   <Route path="/layout" component={Layout}/>
                   <Route  path="/visites" component={Visites}/>
                   <Route  path="/admins" component={Admin}/>
