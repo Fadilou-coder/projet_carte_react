a0a117b8c929c2d4abf2765145154252f0ed87fd
cbag1 <bacargoudiaby@gmail.com>
2022-03-07 11:03:10 +0000

    visite

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginComponent from "./components/loginComponent/LoginComponent";
 import Login from './components/login1/Login';
 import Navbar from './components/navbar/Navbar';
+import Visite from './components/visite/Visite';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
           <Route path="/login" component={LoginComponent} />
           <Route path="/login1" component={Login} />
           <Route path="/navbar" component={Navbar} />
+          <Route path="/visites" component={Visite} />
+
 
         </Switch>
       </Router>
