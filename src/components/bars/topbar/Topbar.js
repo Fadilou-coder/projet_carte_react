35c648fd8ac1ab344a71b3f2f7cec8e54ec33816
Mamadou Sylla <momosylla1998@gmail.com>
2022-03-10 13:22:54 +0000

    new push

--- src/components/bars/topbar/Topbar.js
+++ src/components/bars/topbar/Topbar.js
@@ -5,6 +5,9 @@ import MenuIcon from '@material-ui/icons/Menu'
 import styled from "styled-components";
 import { Typography, FormControl, OutlinedInput, InputAdornment } from "@mui/material";
 import { SearchOutlined } from '@mui/icons-material';
+import Popover from '@mui/material/Popover';
+import Button from '@mui/material/Button';
+
 
 const Topbar = ({ funcSetIsMobile }) => {
     const classes = TopbarStyle();
@@ -19,6 +22,21 @@ const Topbar = ({ funcSetIsMobile }) => {
   display: flex;
   align-items: center;
 `;
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
+
     return (
         <div>
             <AppBar position="fixed" sx={{ with: "100%" }}   >
@@ -52,16 +70,34 @@ const Topbar = ({ funcSetIsMobile }) => {
                         </FormControl>
                     </div>
                     <div className={classes.avatar}>
-                        <AvatarContainer>
-                            <AvatarLabel>
-                                <Avatar
-                                    style={{ marginRight: "14px" }}
-                                    alt="Jack Sparrow"
-                                    src="https://images.pexels.com/photos/6386956/pexels-photo-6386956.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                                />
-                                <Typography variant="body2" style={{ color: "white" }} >Baye Niass</Typography>
-                            </AvatarLabel>
-                        </AvatarContainer>
+                        <Button aria-describedby={id}  onClick={handleClick}>
+                            <AvatarContainer>
+                                <AvatarLabel>
+                                    <Avatar
+                                        style={{ marginRight: "14px" }}
+                                        alt="Jack Sparrow"
+                                        src="https://images.pexels.com/photos/6386956/pexels-photo-6386956.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                                    />
+                                    <Typography variant="body2" style={{ color: "white" }} >Baye Niass</Typography>
+                                </AvatarLabel>
+                            </AvatarContainer>
+                                <Popover
+                                    id={id}
+                                    open={open}
+                                    anchorEl={anchorEl}
+                                    onClose={handleClose}
+                                    anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'right',
+                                      }}
+                                      transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                      }}
+                                    >
+                                    <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+                                </Popover>
+                        </Button>
                     </div>
                 </Toolbar>
             </AppBar>
