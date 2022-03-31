9b2181981ad0d1ed30bb374f1d2aa584a7ed4537
Fadilou-coder <syfadilou3@gmail.com>
2022-03-31 12:24:32 +0000

    fadilou

--- src/components/bars/leftbar/LeftBarDesign.js
+++ src/components/bars/leftbar/LeftBarDesign.js
@@ -55,6 +55,7 @@ const LeftBarDesign = () => {
             <div className={classes.logoDiv}>
                 <Avatar alt='Sonatel Academy' src={sonatelLogo} variant="square"
                     className={classes.logoStyle}
+                    style={{"& .MuiAvatar-root": {width: '100%'}}}
                 />
             </div>
             {myTab}
