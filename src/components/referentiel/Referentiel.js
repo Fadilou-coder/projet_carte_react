f7264e27b2bb1e0171f6a880544a0b5dbecb7aa3
Ahmadou Asna Khadim Gueye <asnaa.gueye@gmail.com>
2022-04-21 09:08:24 +0000

    asna

--- src/components/referentiel/Referentiel.js
+++ src/components/referentiel/Referentiel.js
@@ -7,8 +7,32 @@ const Referentiel = () => {
     return (
         <Layout>
             <Typography variant="h1">Referentiel</Typography>
+
+
+            <div>
+
+                <Typography
+                    variant='h5'
+                    style={{
+                        marginBottom: "20px",
+                        borderLeft: "6px solid #000000",
+                        color: "#000000",
+                        paddingLeft: "20px",
+                        fontWeight: "bolder"
+                    }}>
+                    LISTE DES STRUCTURES
+                </Typography>
+
+
+
+            </div>
         </Layout>
-    )             
+    )
+
+
+
 }
+
+
 
 export default Referentiel
