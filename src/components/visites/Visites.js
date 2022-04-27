2c003e4d0c5b4be5aa28814cfcd083400822b907
cbag1 <bacargoudiaby@gmail.com>
2022-04-27 12:26:46 +0000

    Affichage en Français No Rows Datagrid

--- src/components/visites/Visites.js
+++ src/components/visites/Visites.js
@@ -61,6 +61,31 @@ export const Visites = () => {
 
 
 
+    // Custom Datagrid No Rows show
+
+    function CustomNoRowsOverlay() {
+        return (
+
+            <Grid sx={{ display: "flex", justifyContent: "center", }}>
+                <div>
+                    <Box sx={{ mt: 1, fontWeight: "bold", fontSize: "20px" }}>
+                        Tableau Vide
+                    </Box>
+                    <Box sx={{ width: "80px" }} >
+
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+                        </svg>
+
+                    </Box>
+                </div>
+            </Grid >
+
+        );
+    }
+
+
+
 
     // Custom Pagination
     function CustomPagination() {
@@ -553,6 +578,7 @@ export const Visites = () => {
                                         rowsPerPageOptions={[5, 10, 20]}
                                         components={{
                                             Pagination: CustomPagination,
+                                            NoRowsOverlay: CustomNoRowsOverlay,
                                             // Toolbar: CustomToolbar,
                                         }}
                                         loading={loading}
